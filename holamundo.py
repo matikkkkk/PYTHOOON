0f45f50f5f844bab1ec663faa3bd11b5f3ae341a
matikkkkk <matydiscord2@gmail.com>
2025-04-15 10:23:26 -0400

    INPUT Y CAMBIOS avances

--- holamundo.py
+++ holamundo.py
@@ -1,2 +1,29 @@
-print("holamundo")
+#variables
+# nombre="matias"
+# edad=65
+# nombre=input()    
+
+
+#ejemplo de concatenacion 
+# print("hola mundo me llamo", nombre, "y su edad es", edad)
+
+# print("hola mundo me llamo", nombre, "y su edad es", edad, "y en 5 años será", edad+5)
+
+#Hay dos formas forma 1:
+# print("ingrese dos numeros")
+# n1=int(input())
+# n2=int(input())
+# print("la suma de", n1, "y", n2, "es", n1+n2)
+
+#Forma 2
+# n1=int(input("ingrese un  numero "))
+# n2=int(input("ingrese un  numero "))
+
+# print("la suma de", n1, "y", n2, "es", n1+n2)
+
+
+
+
+
+
 
