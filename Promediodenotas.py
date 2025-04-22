e5372ff13a768ae423961a8e078724ff062409e7
matikkkkk <matydiscord2@gmail.com>
2025-04-22 09:35:21 -0400

    cambiosss

---
+++ Promediodenotas.py
@@ -0,0 +1,14 @@
+
+print("ingrese 3 numeros")
+
+n1=float(input())
+n2=float(input())
+n3=float(input())
+prom=(n1+n2+n3)/3
+print("el promedio de", n1, n2, n3 ,"es", prom) 
+
+if prom>=4.0:
+    print("el estudiante aprobó")
+else:
+    print("el estudiante reprobó")
+    
---
+++ if python.py
@@ -0,0 +1,10 @@
+print("ingrese su nombre")
+nombre=input()
+print("ingresa tu edad")
+edad=int(input())
+
+if edad>=18:
+    print("tu nombre es", nombre, "y eres mayor de edad")
+else:
+    print("tu nombre es", nombre, "y eres menor de edad")
+
