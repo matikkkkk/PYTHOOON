f79c60736cdd89b68aa44a504039fe021a45cb1b
matikkkkk <matydiscord2@gmail.com>
2025-04-25 23:16:57 -0400

    aaaaaa

--- while.py
+++ while.py
@@ -6,18 +6,33 @@
 #     time.sleep(1)
 #     num-=1
 
-# resp="no"
-# while resp!="si":
+#resp="no"
+#while resp!="si":
+    #resp=input("desea salir del sistema?")
 
-#     resp=input("desea salir del sistema?")
+#clave=2233
+#contraseña=int(input("ingrese su contraseña "))
+#while clave!=contraseña:
+#    contraseña=int(input("ingrese su contraseña"))
+#print("clave correcta")
 
-clave=2233
-contraseña=int(input("ingrese su contraseña "))
-while clave!=contraseña:
-    print("error clave invalida")
-    contraseña=int(input("ingrese su contraseña"))
-print("clave correcta")
+clave=2255
+intento=1
+limite=10
+
+contrasena=int(input("ingrese su coontrasena "))
+print("intento nro", intento)
+
+while  contrasena!= clave and intento<limite:
+    print("error")
+    intento=intento+1
+    contrasena=int(input("ingrese su coontrasena "))
+    print("intento nro", intento)
+    
+if contrasena == clave:
+    print("Contraseña correcta")
+else:
+    print("muchos intentos...")
 
 
 
-               
