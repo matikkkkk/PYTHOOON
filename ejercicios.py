dd6a7019482db03276355d20e52cf1059ff19e4b
matikkkkk <matydiscord2@gmail.com>
2025-04-29 19:45:23 -0400

    ejercicios

---
+++ ejercicios.py
@@ -0,0 +1,45 @@
+#EJERCICIO1
+
+# num=int(input("ingrese un numero "))
+# for i in range (num):
+#     if i % 2==0:
+#         print("el numero", i, "es par")
+#     else:
+#         print("su numero", i,  "es impar")
+
+#EJERCICIO2
+# suma=0
+# cont=0
+# op=0
+# while op!=2:
+#     print("1.ingresar un num 2. salir ")
+#     op=int(input())
+#     if op==1:
+#         num=int(input("ingrese el numero "))
+#         suma+=num
+#         cont+=1
+# print("la cantidad de numeros ingresados es ", cont)
+# print("la suma de los numeros es ", suma)       
+
+#EJERCICIO3
+# from random import randint
+# a=0
+# b=0
+# while a*b<50:
+#     a=int(input("ingrese el valor de a : "))
+#     b=randint(2,8)
+#     print(b)
+#     print("la multiplicacion de ", a, "x", b, "es", a*b)
+# print("logro la meta")
+
+#EJERCICIO4
+# num=-1
+# while num<0:
+#     num=int(input("ingrese un numero: "))
+#     if num<0:
+#         print("ingresó un numero negativo intentar nuevamente ")
+# total=num*5
+# total=total-8
+# total=total+3
+# total=total/2
+# print("el valor del total es ", total)
