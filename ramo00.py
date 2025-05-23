1ed742a73d6e4fb7c8d4dccf916e053d8568c197
matikkkkk <matydiscord2@gmail.com>
2025-05-23 00:33:10 -0400

    ramo00

--- ramo00.py
+++ ramo00.py
@@ -1,6 +1,31 @@
 
 decimas=0
 talleres=4
+Tasistidos=0
 cantrojos=int(input("Cuantos ramos rojos hay en el curso:  "))
-for i in range(cantrojos):
+for i in range(1, talleres + 1):
+    resp=int(input(f'''usted asistio al taller nro {i}
+                    1.- si 
+                    2.- no
+'''))    
+    if resp==1:
+        Tasistidos+=1
+decimas=Tasistidos*0.3
+
+if cantrojos<=1:
+    print("usted tiene la bendicion del profesor")
+    print("tienes esta cantidad de decimas: ", decimas)
+    notafi=float(input("ingrese su nota final: "))
+    notafi+=decimas
+    print("su nota final con decimas es: ", round(notafi, 2))
+    if notafi>=4.0:
+        print("Usted aprobo")
+    else:
+        print("usted reprobo")
+else:
+    print("no se le puede ayudar tiene demasiados rojos")
+
+
+
+
     
