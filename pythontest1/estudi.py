007a27a2f3c86e48f294def459a33886d93e8c79
matikkkkk <matydiscord2@gmail.com>
2025-06-26 01:54:45 -0400

    mmmm

---
+++ pythontest1/estudi.py
@@ -0,0 +1,34 @@
+estudiantes={
+    1: {
+            "nombre": "ricardo",
+            "edad" : 18,
+            "curso" : "cuarto medio",
+            "promedio": 6.3 
+            }
+}
+while True:
+    opc=int(input("""
+    1.- Ingresar 
+    2.- Mostrar 
+    3.- Actualizar
+    4.- Borrar 
+    6.- Salir"""))
+    match opc:
+        case 1:
+            nom=input("ingresa nombre del estudiante")
+            edad=int(input("ingresa edad"))
+            curso=(input("ingresa curso"))
+            prome=float(input("ingresa promedio"))
+            largo=list(estudiantes.keys())[-1]
+            estudiantes[largo+1]={
+            "nombre": nom,
+            "edad" : edad,
+            "curso" : curso,
+            "promedio" : prome
+            }
+        case 2:
+            for k, v in estudiantes.items():
+                print(k, v)
+        case 3:
+            for k, v in estudiantes.items():
+                print(k, v)
